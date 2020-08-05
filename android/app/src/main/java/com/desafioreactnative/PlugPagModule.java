600b0b5b2d09ae2c07375abb337a40669ff41041
Andreazo Silva <andreazocoisas@gmail.com>
2020-08-05 15:41:06 -0300

    Implemented payment with credt card

--- android/app/src/main/java/com/desafioreactnative/PlugPagModule.java
+++ android/app/src/main/java/com/desafioreactnative/PlugPagModule.java
@@ -13,6 +13,10 @@ import com.facebook.react.bridge.WritableMap;
 
 import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPag;
 import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagAppIdentification;
+import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagTransactionResult;
+import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagPaymentData;
+import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagActivationData;
+import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagInitializationResult;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,23 +40,108 @@ public class PlugPagModule extends ReactContextBaseJavaModule {
         return "PlugPagModule";
     }
 
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put("PAYMENT_CREDIT", PlugPag.TYPE_CREDITO);
+        constants.put("PAYMENT_DEBIT", PlugPag.TYPE_DEBITO);
+        constants.put("PAYMENT_VOUCHER", PlugPag.TYPE_VOUCHER);
+        constants.put("INSTALLMENT_TYPE_A_VISTA", PlugPag.INSTALLMENT_TYPE_A_VISTA);
+        constants.put("INSTALLMENT_TYPE_PARC_VENDEDOR", PlugPag.INSTALLMENT_TYPE_PARC_VENDEDOR);
+        return constants;
+    }
+
     @ReactMethod
-    public void getApplicationVersion(Promise promise) {
+    public void isAuthenticated(ReadableMap request, Promise promise) {
+        String appName = request.getString("appName");
+        String appVersion = request.getString("appVersion");
+
+        PlugPagAppIdentification appIdentification = new PlugPagAppIdentification(appName, appVersion);
+        final PlugPag plugpag = new PlugPag(reactContext, appIdentification);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Callable<Boolean> callable = new Callable<Boolean>() {
+            @Override
+            public Boolean call() {
+                return plugpag.isAuthenticated();
+            }
+        };
+        Future<Boolean> future = executor.submit(callable);
+        executor.shutdown();
+
         final WritableMap map = Arguments.createMap();
-        PlugPag plugpag = new PlugPag(getReactApplicationContext(), new PlugPagAppIdentification("AppDemo","1.0"));
-        PlugPagAppIdentification identification = plugpag.getAppIdentification();
-        String version = identification.getVersion();
-        promise.resolve(version);
+        try {
+            map.putBoolean("isAuthenticated", future.get());
+            promise.resolve(map);
+        } catch (InterruptedException e) {
+            promise.reject("000", e.getMessage());
+        } catch (ExecutionException e) {
+            promise.reject("000", e.getMessage());
+        }
+
     }
 
-      @ReactMethod
-    public void getLibVersion(Promise promise) {
-        final WritableMap map = Arguments.createMap();
-        PlugPag plugpag = new PlugPag(getReactApplicationContext(), new PlugPagAppIdentification("AppDemo","1.0"));
-        String libVersion = plugpag.getLibVersion();
-        promise.resolve(libVersion);
-    }
+    @ReactMethod
+    public void doPaymentCreditCrad(ReadableMap request, Promise promise) throws Exception{
+        String appName = request.getString("appName");
+        String appVersion = request.getString("appVersion");
+        String activationCode = request.getString("activationCode");
+        String amount = request.getString("amount");
+        String SalesCode = request.getString("salesCode");
 
+        // Define os dados do pagamento
+        PlugPagPaymentData paymentData = new PlugPagPaymentData(PlugPag.TYPE_CREDITO, Integer.valueOf(amount),
+                PlugPag.INSTALLMENT_TYPE_A_VISTA, 1, SalesCode);
+
+        // Cria a identificação do aplicativo
+        PlugPagAppIdentification appIdentification = new PlugPagAppIdentification(appName, appVersion);
+
+        // Cria a referência do PlugPag
+        PlugPag plugpag = new PlugPag(reactContext, appIdentification);
+
+        ExecutorService connectionExecutor = Executors.newSingleThreadExecutor();
+        Callable<PlugPagInitializationResult> connectionCallable = new Callable<PlugPagInitializationResult>() {
+            @Override
+            public PlugPagInitializationResult call() {
+                return plugpag.initializeAndActivatePinpad(new PlugPagActivationData(activationCode));
+
+            }
+        };
+        Future<PlugPagInitializationResult> initResult = connectionExecutor.submit(connectionCallable);
+        connectionExecutor.shutdown();
+
+        // Ativa terminal e faz o pagamento
+        PlugPagInitializationResult initResultCode = initResult.get();
+
+        final WritableMap map = Arguments.createMap();
+
+        if (initResult.get() != null && initResultCode.getResult() == PlugPag.RET_OK) {
+
+            ExecutorService paymentExecutor = Executors.newSingleThreadExecutor();
+            Callable<PlugPagTransactionResult> paymentCallable = new Callable<PlugPagTransactionResult>() {
+                @Override
+                public PlugPagTransactionResult call() {
+                    return plugpag.doPayment(paymentData);
+                }
+            };
+
+            Future<PlugPagTransactionResult> transactionResult = paymentExecutor.submit(paymentCallable);
+
+            if (transactionResult.get() != null && transactionResult.get().getResult() == PlugPag.RET_OK) {
+
+                if (transactionResult.get() != null) {
+                    map.putInt("code", transactionResult.get().getResult());
+                    map.putString("msg", transactionResult.get().getMessage());
+                    promise.resolve(map);
+                }
+            } else {
+                promise.reject("erro", "Something wrong with transaction");
+            }
+
+        } else {
+            promise.reject("erro", "Can't conect the device");
+        }
+    }
 
     @ReactMethod
     public void show(String message) {
--- src/modules/PluPagModule/index.js
+++ src/modules/PluPagModule/index.js
@@ -1,2 +1,46 @@
 import { NativeModules } from 'react-native';
-module.exports = NativeModules.PlugPagModule;
+
+const { PlugPagModule } = NativeModules;
+
+export const RNPlugPag = {
+    appName: '',
+    appVersion: '',
+    activationCode: '',
+
+    PAYMENT_CREDIT: PlugPagModule.PAYMENT_CREDIT,
+    PAYMENT_DEBIT: PlugPagModule.PAYMENT_DEBIT,
+    PAYMENT_VOUCHER: PlugPagModule.PAYMENT_VOUCHER,
+
+    INSTALLMENT_TYPE_A_VISTA: PlugPagModule.INSTALLMENT_TYPE_A_VISTA,
+    INSTALLMENT_TYPE_PARC_VENDEDOR: PlugPagModule.INSTALLMENT_TYPE_PARC_VENDEDOR,
+
+    setAppInfo(appName = '', appVersion = '') {
+        this.appName = appName;
+        this.appVersion = appVersion;
+    },
+
+    setActivationCode(activationCode = '') {
+        this.activationCode = activationCode;
+    },
+
+
+    isAuthenticated() {
+        if (!this.appName || !this.appVersion) {
+            throw new Error('You must set appName and appVersion before call isAuthenticated method.');
+        }
+
+        return PlugPagModule.isAuthenticated({ appName: this.appName, appVersion: this.appVersion });
+    },
+    doPaymentCreditCrad(request) {
+
+        if (!this.appName || !this.appVersion || !this.activationCode) {
+            throw new Error('You must set appName, appVersion and activationCode before call checkout method.');
+        }
+
+        return PlugPagModule.doPaymentCreditCrad({ ...request, appName: this.appName, appVersion: this.appVersion, activationCode: this.activationCode })
+
+    }
+};
+
+
+
